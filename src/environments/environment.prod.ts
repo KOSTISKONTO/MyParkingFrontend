6b6243e691b9e955f3fde08d340e251839f74d1f
konst <konstantinos.konto@gmail.com>
2026-03-22 02:30:08 +0200

    api_url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   googleMapsKey: 'AIzaSyD0969gQ2ExepQW7IY5jypCLBCIn4DsKFc',
-  api_url:'https://myparkingfrontent-fchsarc6djhub7b7.canadacentral-01.azurewebsites.net',
+  api_url:'myparkingbackend-bka6eefxgkhddbhr.canadacentral-01.azurewebsites.net',
   wsUrl: 'ws:///myparkingfrontent-fchsarc6djhub7b7.canadacentral-01.azurewebsites.net'
 };
