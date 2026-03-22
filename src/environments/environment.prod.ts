ce1ae1363f562d679a8b2218398b70e0669e1b37
konst <konstantinos.konto@gmail.com>
2026-03-22 11:28:39 +0200

    api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  googleMapsKey: 'AIzaSyD0969gQ2ExepQW7IY5jypCLBCIn4DsKFc',
-  api_url:'myparkingbackend-bka6eefxgkhddbhr.canadacentral-01.azurewebsites.net',
-  wsUrl: 'ws:///myparkingfrontent-fchsarc6djhub7b7.canadacentral-01.azurewebsites.net'
+  googleMapsKey: '...',
+  api_url: 'https://myparkingbackend-bka6eefxgkhddbhr.canadacentral-01.azurewebsites.net',
+  wsUrl: 'wss://myparkingbackend-bka6eefxgkhddbhr.canadacentral-01.azurewebsites.net'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   googleMapsKey: 'AIzaSyD0969gQ2ExepQW7IY5jypCLBCIn4DsKFc',
-  api_url: 'http://localhost:8080',
-  wsUrl: 'ws://localhost:8080'
+  api_url: 'https://myparkingbackend-bka6eefxgkhddbhr.canadacentral-01.azurewebsites.net',
+  wsUrl: 'wss://myparkingbackend-bka6eefxgkhddbhr.canadacentral-01.azurewebsites.net'
 };
